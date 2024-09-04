1d79904846b7858ab7e77a31c757e69e2efc8143
AbhijeetWaghmare <abhijeetWaghmare2000@gmail.com>
2024-09-04 22:49:04 +0530

    first commit

---
+++ app/chains.py
@@ -0,0 +1,64 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(
+            model=os.getenv("MODEL"),
+            temperature=0,
+            groq_api_key=os.getenv("GROQ_API_KEY"),
+        )
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+        ### SCRAPE TEXT FOR WEBSITE:
+        {page_data}
+        
+        ### INSTRUCTION:
+        The scraped text is from the career's page of a company website.
+        Your job is to extract the job postings and return them in JSON format containing the following keys: 'role', 'experience','skills','description'.
+        Only return the valid JSON.
+        ### NO PREAMBLE
+        """
+        )
+
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+
+        except OutputParserException:
+            raise OutputParserException("Content is too big. Unable to parse job.")
+        return res if isinstance(res, list) else [res]
+
+    def write_email(self, job, links):
+        prompt = PromptTemplate.from_template(
+            """
+        ### Job Description:
+        {job_description}
+        
+        ### INSTRUCTION:
+        You are Abhijeet, a software Engineer. With 1.5 Years of experience in Backend development. Started as chatbot engineer design in and developing chatbots and then transitioned into python backend development. Mostly worked on building APIs in django rest framework and then completed AWS certification. Previous company let go due to financial hardship. I started looking for a job and was not able to find roles and financial expectations. 
+Started learning data science and upskilled in SQL,Tableau , PowerBI, Excel,  data cleaning , data visualization , EDA etc. Strong analytical and problem-solving capabilities. A quick learner with an unwavering passion for staying abreast of emerging technologies.
+
+Your job is to write an email to the hiring manager regarding the job mentioned above describing the Abhijeet's capability and add some project links to showcase portfolio{links_list} .
+       
+ Remember you are Abhijeet.
+
+         Do not provide PREAMBLE.
+         ### EMAIL (NO PREAMBLE). 
+        """
+        )
+
+        chain_email = prompt | self.llm
+        res = chain_email.invoke(input={"job_description": job, "links_list": links})
+        return res.content
---
+++ app/main.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pyperclip
+from chains import Chain
+from portfolio import Portfolio
+from langchain_community.document_loaders import WebBaseLoader
+
+from utils import clean_text
+
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
+def create_streamlit_app(llm ,portfolio):
+    st.title("📧 Cold Mail generator")
+    st.subheader("Turn Job Listings into Job Offers with the Ultimate Cold Email Generator!")
+
+    url_input = st.text_input("Enter URL: ", placeholder="Enter job description link ")
+    submit_input = st.button("Submit")
+
+    
+
+    if submit_input:
+        
+        try:
+            loader = WebBaseLoader(url_input)
+            data = clean_text(loader.load().pop().page_content)
+            portfolio.load_portfolio()
+            jobs = llm.extract_jobs(data)
+            for job in jobs:    
+                skills = job.get('skills',[])
+                links = portfolio.query_links(skills)
+                email = llm.write_email(job, links)
+                st.code(email, language="markdown")
+                
+        except Exception as e:
+            st.error(f" Error Occurred: {e}")
+
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide",page_title="Cold Email Generator", page_icon="📧")
+    
+    create_streamlit_app(chain, portfolio)
